623d0249c653dee9a6cda91622dcfe3578802dd9
dansturm-sandwich <dan@sandwich.co>
2023-05-25 10:34:40 -0700

    v03

--- nuke_annotationShot.py
+++ nuke_annotationShot.py
@@ -1,45 +1,51 @@
+import nuke
 import os
 import os.path
 from datetime import date
-date = date.today().strftime('%y%m%d')
 
-frame = f'{(int(nuke.frame())):04}'
+def annotationNukeX():
+	tdate = date.today().strftime('%y%m%d')
 
-fileEnd = str(frame) + ".jpg"
+	frame = f'{(int(nuke.frame())):04}'
 
-viewer = nuke.activeViewer()
-viewerNode = viewer.node()
-activeBuffer = viewer.activeInput()
-inputNode = viewerNode.input(activeBuffer)
+	fileEnd = str(frame) + ".jpg"
 
-topNode = nuke.toNode(nuke.tcl('full_name [topnode {0}]'.format(inputNode.name())))
-filePath = topNode['file'].getValue()
-fwi = topNode.width()
-fhi =  topNode.height()
+	viewer = nuke.activeViewer()
+	viewerNode = viewer.node()
+	activeBuffer = viewer.activeInput()
+	inputNode = viewerNode.input(activeBuffer)
 
-
-filename = filePath.split('/')[-1]
-filename = filename.split('.')[0]
-fullfiilename = filename + "." + fileEnd
-
-pPath = filePath.split('online')[0]
-dPath = pPath + "online/_project_files/_dailies/_notes/" + str(date)
+	topNode = nuke.toNode(nuke.tcl('full_name [topnode {0}]'.format(inputNode.name())))
+	filePath = topNode['file'].getValue()
+	fwi = topNode.width()
+	fhi =  topNode.height()
 
 
-dircmd = 'mkdir -p "' + dPath + '"'
-os.system(dircmd)
+	filename = filePath.split('/')[-1]
+	filename = filename.split('.')[0]
+	fullfiilename = filename + "." + fileEnd
+
+	pPath = filePath.split('online')[0]
+	dPath = pPath + "online/_project_files/_dailies/_notes/" + str(tdate)
 
 
-fpath = dPath + "/" + fullfiilename
-
-nuke.activeViewer().node().capture(fpath)
-
-
-os.system( 'sips -s profile /Library/ColorSync/Profiles/Displays/StudioDisplay-7B124C67-2DD2-8F2D-1452-F1C958A0C9F4.icc "' + fpath + '"')
-
-os.system( 'sips --resampleHeight "' + str(fhi) + '" "' + fpath + '"')
-
-os.system( 'sips --cropToHeightWidth "' + str(fhi) + '" "' + str(fwi) + '" "' + fpath + '"')
+	dircmd = 'mkdir -p "' + dPath + '"'
+	os.system(dircmd)
 
 
-os.system('open "' + fpath + '"')
+	fpath = dPath + "/" + fullfiilename
+
+	nuke.activeViewer().node().capture(fpath)
+
+
+	os.system( 'sips -s profile /Library/ColorSync/Profiles/Displays/StudioDisplay-7B124C67-2DD2-8F2D-1452-F1C958A0C9F4.icc "' + fpath + '"')
+
+	os.system( 'sips --resampleHeight "' + str(fhi) + '" "' + fpath + '"')
+
+	os.system( 'sips --cropToHeightWidth "' + str(fhi) + '" "' + str(fwi) + '" "' + fpath + '"')
+
+	os.system('open "' + dPath + '"')
+
+	os.system('open "' + fpath + '"')
+
+
